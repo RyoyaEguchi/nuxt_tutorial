44c9656e235b7ac11efdaee13cbf77d3368c2f98
江口凌也 <eguchiryoya@eguchiryounarinoMacBook-Pro.local>
2022-03-20 23:11:12 +0900

    ページ切り替えまで

--- pages/parent.vue
+++ pages/parent.vue
@@ -4,8 +4,8 @@
     <NuxtLink to="/">Home</NuxtLink>
     <NuxtLink to="/menu">Menu</NuxtLink>
     <NuxtLink to="/parent">Parent</NuxtLink>
-    <NuxtLink to="/parent/child">child</NuxtLink>
-    <NuxtLink to="/parent/child2">child 2</NuxtLink>
+    <NuxtLink to="/1parent/child">child</NuxtLink>
+    <NuxtLink to="/1parent/child2">child 2</NuxtLink>
     <NuxtChild />
   </div>
 </template>
